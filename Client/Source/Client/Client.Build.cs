9fa14d46c1fb3a91b64f24b30d2a8daeade24af3
PGH\qkrru <qkrrudgh30@naver.com>
2024-10-02 23:51:43 +0900

    feat: Add ProtobufCore moduel to Unreal Client Project

--- Client/Source/Client/Client.Build.cs
+++ Client/Source/Client/Client.Build.cs
@@ -8,6 +8,17 @@ public class Client : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] 
+		{ 
+			"Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput",
+
+			"Sockets", "Networking",
+			
+		});
+
+		PrivateDependencyModuleNames.AddRange(new string[] 
+		{ 
+			"ProtobufCore", // 언리얼 엔진 내부적으로는 프로토버프 3.18 버전을 고정적으로 사용중. 우리는 1.17버전을 사용중이라, 우리만의 모듈을 추가한것.
+		});
 	}
 }
