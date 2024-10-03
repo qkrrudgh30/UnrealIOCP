9f40dddf07bf1cba0725484009c430621e66d718
PGH\qkrru <qkrrudgh30@naver.com>
2024-10-03 23:36:02 +0900

    feat: Unreal client project와 ServerCore project 연결

--- Client/Source/Client/Client.Build.cs
+++ Client/Source/Client/Client.Build.cs
@@ -1,5 +1,6 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class Client : ModuleRules
@@ -20,5 +21,17 @@ public class Client : ModuleRules
 		{ 
 			"ProtobufCore", // 언리얼 엔진 내부적으로는 프로토버프 3.18 버전을 고정적으로 사용중. 우리는 1.17버전을 사용중이라, 우리만의 모듈을 추가한것.
 		});
+		
+		PublicIncludePaths.AddRange(new string[] 
+		{ 
+			Path.Combine(ModuleDirectory, "../../../Server/ServerCore"),
+		});
+		
+		PublicAdditionalLibraries.AddRange(new string[] 
+		{ 
+			Path.Combine(ModuleDirectory, "../../../Server/Libraries/ServerCore/Debug", "ServerCore.lib"),
+			Path.Combine(ModuleDirectory, "../../../Server/Libraries/ServerCore/Release", "ServerCore.lib"),
+		});
+		
 	}
 }
